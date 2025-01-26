8c602b704e5d2a3389a619a51650e865773b06e5
TOM-TITO-code <titotom934@gmail.com>
2025-01-26 16:26:13 +0700

    prepare integrate api

---
+++ lib/api/constance_api.dart
@@ -0,0 +1,7 @@
+
+import 'package:dio/dio.dart';
+import 'package:get_storage/get_storage.dart';
+
+const baseUrl =  "http://192.168.20.140:3000/v1";
+final dio = Dio();
+final storage = GetStorage();
---
+++ lib/models/login_model.dart
@@ -0,0 +1,42 @@
+class LoginModel {
+  String? token;
+  String? role;
+  User? user;
+
+  LoginModel({this.token, this.role, this.user});
+
+  LoginModel.fromJson(Map<String, dynamic> json) {
+    token = json['token'];
+    role = json['role'];
+    user = json['user'] != null ? new User.fromJson(json['user']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['token'] = this.token;
+    data['role'] = this.role;
+    if (this.user != null) {
+      data['user'] = this.user!.toJson();
+    }
+    return data;
+  }
+}
+
+class User {
+  String? firstName;
+  String? lastName;
+
+  User({this.firstName, this.lastName});
+
+  User.fromJson(Map<String, dynamic> json) {
+    firstName = json['firstName'];
+    lastName = json['lastName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['firstName'] = this.firstName;
+    data['lastName'] = this.lastName;
+    return data;
+  }
+}
